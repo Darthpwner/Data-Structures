1b7323ea014a1326d0529de81dea93094d0f6630
Matthew Allen Lin <Darthpwner@Matthews-MacBook-Pro.local>
2015-10-30 23:44:19 -0700

    Need to do the reverse function

--- Linked-List/Linked-List/main.cpp
+++ Linked-List/Linked-List/main.cpp
@@ -177,6 +177,10 @@ SinglyLinkedList::~SinglyLinkedList() {
     }
 }
 
+void SinglyLinkedList::reverse() {
+    //TODO
+}
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
