1c7e140d29da66d1ece9a2758bad7b80302b27be
Matthew Allen Lin <Darthpwner@Matthews-MacBook-Pro.local>
2015-10-31 00:40:41 -0700

    Debug the stack

--- Stack/Stack/main.cpp
+++ Stack/Stack/main.cpp
@@ -10,20 +10,23 @@
 #include <string>
 using namespace std;
 
+const int SIZE = 100;
+
 class Stack {   //Stack of ints
 public:
     Stack();    //c'tor
     ~Stack();   //d'tor
     void push(int i);
     int pop();
-    bool is_empty();
-    int top();
+    bool is_empty() const;
+    int top() const;
 private:
-    
+    int m_stack[SIZE];
+    int m_top;
 };
 
 Stack::Stack() {
-    
+    m_top = 0;
 }
 
 Stack::~Stack() {
@@ -31,23 +34,41 @@ Stack::~Stack() {
 }
 
 void Stack::push(int i) {
-    
+    if (m_top >= SIZE) return;  //Overflow
+    m_stack[m_top] = i;
+    m_top++;
 }
 
 int Stack::pop() {
-    return -69;
+    if(m_top == 0) return -1;   //Underflow
+    m_top--;
+    return m_stack[m_top];
 }
 
-bool Stack::is_empty() {
+bool Stack::is_empty() const {
+    if(m_top == 0) return true;
     return false;
 }
 
-int Stack::top() {
-    return -69;
+int Stack::top() const {
+    return m_stack[m_top];
+}
+
+void printTop(const Stack& c) {
+    cout << c.top() << endl;
 }
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    Stack s;
+    s.push(1);
+    printTop(s);
+    
+    s.push(2);
+    printTop(s);
+    
+    s.push(3);
+    printTop(s);
+    
     return 0;
 }
