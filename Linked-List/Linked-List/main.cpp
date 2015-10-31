5adb19bff78b646fe582779b50cd00e788e0c7d2
Matthew Allen Lin <Darthpwner@Matthews-MacBook-Pro.local>
2015-10-30 21:12:24 -0700

    Add stuff

---
+++ Linked-List/Linked-List/main.cpp
@@ -0,0 +1,69 @@
+//
+//  main.cpp
+//  Linked-List
+//
+//  Created by Matthew Allen Lin on 10/30/15.
+//  Copyright © 2015 Matthew Allen Lin Software. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Node {
+    //Data
+    string value;
+    
+    Node *next;
+};
+
+class SinglyLinkedList {
+public:
+    SinglyLinkedList();
+    void addToFront(string v);
+    void addToRear(string v);
+    void deleteItem(string v);
+    bool findItem(string v);
+    void printItems();
+    ~SinglyLinkedList();
+    
+    //Additional functions
+    void reverse();
+    
+private:
+    Node* head;
+};
+
+SinglyLinkedList::SinglyLinkedList() {
+    
+}
+
+void SinglyLinkedList::addToFront(string v) {
+    
+}
+
+void SinglyLinkedList::addToRear(string v) {
+    
+}
+
+void SinglyLinkedList::deleteItem(string v) {
+    
+}
+
+bool SinglyLinkedList::findItem(string v) {
+    return false;
+}
+
+void SinglyLinkedList::printItems() {
+    
+}
+
+SinglyLinkedList::~SinglyLinkedList() {
+    
+}
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
