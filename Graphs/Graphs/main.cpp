7fa83dabe6c6f0b13ba8b18a35308328fa8fcce0
Matthew Allen Lin <Darthpwner@Matthews-MacBook-Pro.local>
2015-11-06 02:20:51 -0800

    Fix derived class later

--- Graphs/Graphs/main.cpp
+++ Graphs/Graphs/main.cpp
@@ -15,32 +15,92 @@ using namespace std;
 class Graph {   //Assumes a (undirected) bidirectional graph
 public:
     Graph();
-    void addRelation(int x, int y);
+    virtual void setGraph(int x, int y);
+    virtual void addRelation(int x, int y);
     void printGraph();
-    void deleteRelation(int x, int y);
-    
-private:
+    virtual void deleteRelation(int x, int y);
+protected:
     bool m_graph[N][N];
 };
 
 class DirectedGraph: public Graph {   //Assumes a directed graph
-    
+public:
+    virtual void setGraph(int x, int y);
+    virtual void addRelation(int x, int y);
+    virtual void deleteRelation(int x, int y);
 };
+
+//Adds a directed graph edge from x to y
+void DirectedGraph::setGraph(int x, int y) {
+    m_graph[x][y] = true;
+}
+
+void DirectedGraph::addRelation(int x, int y) {
+    if(x > N - 1) {
+        cout << "x is too large\n";
+        return;
+    } else if (x < 0) {
+        cout << "x needs to be a positive number\n";
+        return;
+    } else if (y > N - 1) {
+        cout << "y is too large\n";
+        return;
+    } else if (y < 0) {
+        cout << "y needs to be a positive number\n";
+        return;
+    }
+    
+    setGraph(x, y);
+}
+
+
+//Deletes a directed graph edge relation from x to y
+void DirectedGraph::deleteRelation(int x, int y) {
+    if(x > N - 1) {
+        cout << "x is too large\n";
+        return;
+    } else if (x < 0) {
+        cout << "x needs to be a positive number\n";
+        return;
+    } else if (y > N - 1) {
+        cout << "y is too large\n";
+        return;
+    } else if (y < 0) {
+        cout << "y needs to be a positive number\n";
+        return;
+    }
+    
+    m_graph[x][y] = false;
+}
 
 int main(int argc, const char * argv[]) {
     Graph g;
     g.printGraph();
     
     g.addRelation(1, 4);
-    g.addRelation(1, 5);    //
+    g.addRelation(1, 5);    //Should display failure message
+    g.addRelation(2, 3);
+    g.deleteRelation(3, 2);
     
-    g.printGraph();
+    g.printGraph(); //Should only show (1, 4) and (4, 1) relationship
+    
+    DirectedGraph dg;
+    cout << "Printed direct graph\n";
+    dg.printGraph();
+    
+    dg.addRelation(1, 4);
 
     return 0;
 }
 
 Graph::Graph() {
     m_graph[N][N] = {false};  //Set the entire graph to false for now (no edge connections at the start)
+}
+
+//Mutator function that sets a relationship between two nodes (bidrectionally)
+void Graph::setGraph(int x, int y) {
+    m_graph[x][y] = true;
+    m_graph[y][x] = true;
 }
 
 //Adds a bidirectional graph edge relation between two nodes
@@ -58,9 +118,8 @@ void Graph::addRelation(int x, int y) {
         cout << "y needs to be a positive number\n";
         return;
     }
-    
-    m_graph[x][y] = true;
-    m_graph[y][x] = true;
+
+    setGraph(x, y); //Assigns values to the graph
 }
 
 //Prints out the 2D representation of the graph. "true" means there is a friendship relation between two nodes
@@ -72,6 +131,8 @@ void Graph::printGraph() {
         }
         cout << "\n";
     }
+    
+    cout << "\n";   //Pad an extra line for cleanliness
 }
 
 //Deletes a bidirectional graph edge relation between two nodes
